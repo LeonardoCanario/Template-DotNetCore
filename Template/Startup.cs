eafd9c4d0469269029a8f32ad6f3b4bb4122dee3
Leonardo Canario <leonardocanario007@gmail.com>
2022-01-02 16:33:57 -0300

    Install and Configure Entity Framework Code in project .Net Core

--- Template/Startup.cs
+++ Template/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Template.Data.Context;
 
 namespace Template
 {
@@ -20,7 +22,12 @@ namespace Template
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
+
+            services.AddDbContext<TemplateContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("TemplateDB")).EnableSensitiveDataLogging());
+
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
