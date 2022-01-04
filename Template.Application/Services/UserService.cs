bc4494c6ada5e2063e80566402173dd09c5ee02c
Leonardo Canario <leonardocanario007@gmail.com>
2022-01-04 18:56:21 -0300

    CRUD finished

--- Template.Application/Services/UserService.cs
+++ Template.Application/Services/UserService.cs
@@ -6,6 +6,7 @@ using Template.Application.Interfaces;
 using Template.Application.ViewModels;
 using Template.Domain.Entities;
 using Template.Domain.Interfaces;
+using Template.Domain.Models;
 
 namespace Template.Application.Services
 {
@@ -27,34 +28,54 @@ namespace Template.Application.Services
 
             _userViewModels = mapper.Map<List<UserViewModel>>(_users);
 
-
-            //foreach (var item in _users)
-            //    _userViewModels.Add(mapper.Map<UserViewModel>(item));
-
-
-
-           // _userViewModels.Add(new UserViewModel { Id = item.Id, Email = item.Email, Name = item.Name, CPF = item.CPF, Phone = item.Phone, });
-
-
-
-
             return _userViewModels;
         }
 
         public bool Post(UserViewModel userViewModel)
         {
-            //User _user = new User
-            //{
-            //    Id = Guid.NewGuid(),
-            //    Email = userViewModel.Email,
-            //    Name = userViewModel.Name,
-            //    CPF = userViewModel.CPF,
-            //    Phone = userViewModel.Phone,
-            //};
             User _user = mapper.Map<User>(userViewModel);
 
             this.userRepository.Create(_user);
             return true;
+        }
+
+        public UserViewModel GetById(string id)
+        {
+            if (!Guid.TryParse(id, out Guid userId))
+                throw new Exception("UserID is not valid");
+
+            User _user = this.userRepository.Find(x => x.Id == userId && !x.IsDeleted);
+            if (_user == null)
+                throw new Exception("User not found");
+
+            return mapper.Map<UserViewModel>(_user);
+        }
+
+        public bool Put(UserViewModel userViewModel)
+        {
+
+            User _user = this.userRepository.Find(x => x.Id == userViewModel.Id && !x.IsDeleted);
+            if (_user == null)
+                throw new Exception("User not found");
+
+            _user = mapper.Map<User>(userViewModel);
+            
+
+            this.userRepository.Update(_user);
+
+            return true;
+        }
+
+        public bool Delete(string id)
+        {
+            if (!Guid.TryParse(id, out Guid userId))
+                throw new Exception("UserID is not valid");
+
+            User _user = this.userRepository.Find(x => x.Id == userId && !x.IsDeleted);
+            if (_user == null)
+                throw new Exception("User not found");
+
+            return this.userRepository.Delete(_user); ;
         }
 
     }
